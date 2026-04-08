bb49dc5e0eaf592c9b94219b3d54450af588053a
Gazzzee <bagasaplik@gmail.com>
2026-04-08 22:22:55 +0700

    setup main

---
+++ lib/services/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: HomeScreen(),
+  ))
+}
