6ce4cb5d57d717d9217f674832dab9c38d463723
BorexxGntng <l200230244@student.ums.ac.id>
2026-04-09 08:53:02 +0700

    Perbaikan path import dan sintaks di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'services/screens/home_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.blue),
     home: HomeScreen(),
-  ))
+  ));
 }
